831f9f6631921c8e29613a1a41a440929fb5e868
scott-ba <pdx.scottb@gmail.com>
2024-08-06 16:13:17 -0700

    To add ROC data generation and plotting.

--- code/genus_analysis.R
+++ code/genus_analysis.R
@@ -105,3 +105,66 @@ composite %>%
 
 ggsave("figures/significant_genera.tiff", width=6, height=4)
 
+# function to use for mapping sens/spec calc over the scores.
+# If score>threshold then TRUE compared to actual TRUE.
+get_sens_spec <- function(threshold, score, actual, direction){
+  
+  # threshold <- 100
+  # score <- test$score
+  # actual <- test$srn
+  # direction <- "greaterthan"
+  
+  predicted <- if(direction == "greaterthan") {
+    score > threshold
+  } else {
+    score < threshold
+  }
+  
+  tp <- sum(predicted & actual)
+  tn <- sum(!predicted & !actual)
+  fp <- sum(predicted & !actual)
+  fn <- sum(!predicted & actual)
+  
+  specificity <- tn / (tn + fp)
+  sensitivity <- tp / (tp + fn)
+  
+  tibble("specificity" = specificity, "sensitivity" = sensitivity)
+}
+
+# generate the ROC data by mapping function onto the sorted scores.
+# row bind to extend the plot to the conrer.
+get_roc_data <- function(x, direction){
+  
+  thresholds <- unique(x$score) %>% sort()
+  
+  map_dfr(.x=thresholds, ~get_sens_spec(.x, x$score, x$srn, direction)) %>%
+    rbind(c(specificity = 0, sensitivity = 1))
+  
+}
+  
+
+  # Map the ROC calc onto a nested data column containing group, srn, and score.
+  
+roc_data <- composite %>%
+  inner_join(sig_genera, by="taxonomy") %>%
+  select(group, taxonomy, rel_abund, fit_result, srn) %>%
+  pivot_wider(names_from=taxonomy, values_from=rel_abund) %>%
+  pivot_longer(cols=-c(group, srn), names_to="metric", values_to="score") %>%
+  # filter(metric == "fit_result") %>%
+  nest(data = -metric) %>%
+  mutate(direction = if_else(metric == "Lachnospiraceae_unclassified",
+                             "lessthan","greaterthan")) %>%
+  mutate(roc_data = map2(.x = data, .y=direction, ~get_roc_data(.x, .y))) %>%
+  unnest(roc_data) %>%
+  select(metric, specificity, sensitivity)
+
+
+# plot ROC by taxonomy name (metric)
+roc_data %>%
+  ggplot(aes(x=1-specificity, y=sensitivity, color=metric)) +
+  geom_line() +
+  geom_abline(slope = 1, intercept = 0, color="gray") +
+  theme_classic()
+
+ggsave("figures/roc_figure.tiff", width=6, height=4)
+
