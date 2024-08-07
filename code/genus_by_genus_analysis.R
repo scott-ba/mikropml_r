6e1c1180c423dc56e57ec75a96a03940a3db1488
scott-ba <pdx.scottb@gmail.com>
2024-08-06 17:32:39 -0700

    Reorginize code.

--- code/genus_by_genus_analysis.R
+++ code/genus_by_genus_analysis.R
@@ -1,61 +1,6 @@
-library(tidyverse)
-library(broom)
-library(ggtext)
 
-set.seed(19760620)
+source("code/genus_process.R")
 
-# adjust group column to char class and all others to dbl.
-
-shared <- read_tsv("raw_data/baxter.subsample.shared",
-                   col_types = cols(Group = col_character(),
-                                    .default = col_double())) %>%
-  rename_all(tolower) %>%
-  select(group, starts_with("otu")) %>%
-  pivot_longer(-group, names_to="otu", values_to="count")
-  
-#load baxter con taxonomy
-#select otu and taxonomy
-#get rid of the number in parentheses and issues with unclassified.
-
-taxonomy <- read_tsv("raw_data/baxter.cons.taxonomy") %>%
-  rename_all(tolower) %>%
-  select(otu, taxonomy) %>%
-  mutate(otu = tolower(otu),
-         taxonomy = str_replace_all(taxonomy, "\\(\\d+\\)", ""),
-         taxonomy = str_replace(taxonomy, ";unclassified", "_unclassified"),
-         taxonomy = str_replace_all(taxonomy, ";unclassified", ""),
-         taxonomy = str_replace_all(taxonomy, ";$", ""),
-         taxonomy = str_replace_all(taxonomy, ".*;", "")
-  )
-
-# join the multiple data frames.
-# add a relative abundance fraction column for each row.
-
-metadata <- read_tsv("raw_data/baxter.metadata.tsv",
-                     col_types=cols(sample = col_character())) %>%
-  rename_all(tolower) %>%
-  rename(group = sample) %>%
-  mutate(srn = dx_bin == "Adv Adenoma" | dx_bin == "Cancer",
-         lesion = dx_bin == "Adv Adenoma" | dx_bin == "Cancer" | dx_bin == "Adenoma")
-
-composite <- inner_join(shared, taxonomy, by="otu") %>%
-  group_by(group, taxonomy) %>%
-  summarize(count = sum(count), .groups="drop") %>%
-  group_by(group) %>%
-  mutate(rel_abund = count / sum(count)) %>%
-  ungroup() %>%
-  select(-count) %>%
-  inner_join(., metadata, by="group")
-
-# look at each genus and find out which genera are sig diff between groups -
-# based on relative abundance.
-# Compare two groups using Wilcox.
-
-# nest list of all columns for every unique taxonomy, to be able to run -
-# a wilcox test by srn for each taxonomy.  Create a new column for the test results.
-# Test for rel_abund differences explained by srn, with the results data
-# fed back into the source data frame. Pipe to broom::tidy before un-nesting.
-# Use false discovery rate to correct the p values.
 sig_genera <- composite %>%
   nest(data = -taxonomy) %>%
   mutate(test = map(.x=data, ~wilcox.test(rel_abund~srn, data=.x) %>% tidy)) %>%
@@ -132,7 +77,7 @@ get_sens_spec <- function(threshold, score, actual, direction){
 }
 
 # generate the ROC data by mapping function onto the sorted scores.
-# row bind to extend the plot to the conrer.
+# row bind to extend the plot to the corner.
 get_roc_data <- function(x, direction){
   
   thresholds <- unique(x$score) %>% sort()
