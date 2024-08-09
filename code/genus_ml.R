d5aecd6feaabc3c83177e222558e85cde4c5ea2a
scott-ba <pdx.scottb@gmail.com>
2024-08-08 17:38:28 -0700

    Add parrallel processing.

--- code/genus_ml.R
+++ code/genus_ml.R
@@ -1,5 +1,10 @@
 source("code/genus_process.R")
 library(mikropml)
+library(tictoc)
+library(furrr)
+
+# plan for future_map. Needs 4.1.1 are greater
+plan("multisession")
 
 # switch logical srn TRUE/FALSE to char srn/healthy for the ml package.
 # drop the group column which is not needed for the ml package.
@@ -26,6 +31,7 @@ srn_genus_preprocess <- preprocess_data(srn_genus_data, outcome_colname = "srn")
 # and the ML method. Feature importance was skipped for now
 # kfold is the number of folds. cv_times is the number of partitions.
 # training_frac is the fraction that is the training set.
+# get_hyperparams_list(srn_genus_preprocess, "glmnet")
 
 test_hp <- list(alpha = 0,
                  lambda = c(0.1, 1, 2, 3, 4, 5, 10))
@@ -42,8 +48,13 @@ get_srn_genus_results <- function(seed){
        seed = seed)
 }
 
-# Using three seeds
-iterative_run_ml_results <- map(c(1,2,3), get_srn_genus_results)
+#add ticktoc. Use seed true in future map if function has seed.
+
+tic()
+iterative_run_ml_results <- future_map(c(1,2,3), get_srn_genus_results,
+                                       .options = furrr_options(seed=TRUE))
+toc()
+
 
 # Extract results and combine the three runs
 performance <- iterative_run_ml_results %>%
@@ -53,13 +64,18 @@ performance <- iterative_run_ml_results %>%
 # plot
 plot_hp_performance(performance$dat, lambda, AUC)
 
+# summarize
 performance$dat %>% 
-  group_by(alpha,lambda) %>% 
-  summarize(mean_AUC = mean(AUC), .groups="drop") %>% 
-#  top_n(n=1, mean_AUC)
-ggplot(aes(x=lambda, y=mean_AUC, color=as.character(alpha))) + geom_line()
+  group_by(alpha,lambda) %>%
+  summarize(mean_AUC = mean(AUC),
+            lquartile = quantile(AUC, prob=0.25),
+            uquartile = quantile(AUC, prob=0.75),
+            .groups="drop") %>% 
+  top_n(n=3, mean_AUC)
+  
+# ggplot(aes(x=lambda, y=mean_AUC, color=as.character(alpha))) + geom_line()
 
-# get_hyperparams_list(srn_genus_preprocess, "glmnet")
+plan("sequential")
 
 
 
